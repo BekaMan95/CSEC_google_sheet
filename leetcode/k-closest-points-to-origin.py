e97b9835aefa5e381d3fbcb42b56222ed0ca035e
BekaMan95 <115992290+BekaMan95@users.noreply.github.com>
2023-06-09 11:39:37 +0300

    Create k-closest-points-to-origin.py

---
+++ leetcode/k-closest-points-to-origin.py
@@ -0,0 +1,15 @@
+import math
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+    
+        distance = []
+        out = []
+        for i in range(len(points)):
+            m = (points[i][0]*points[i][0]) + (points[i][1]*points[i][1])
+            distance.append([math.sqrt(m),[points[i][0],points[i][1]]])
+        distance.sort()
+
+        for i in range(k):
+            out.append(distance[i][1])
+
+        return out
